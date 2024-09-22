76a3441d7d6f3d2a0e0e82de23f4323b4a3515ac
dishaqa <disha.s.chauhan@gmail.com>
2024-09-22 21:02:26 +0530

    Update Assignment_94_doubleclick.java

--- Assignment_94_doubleclick.java
+++ Assignment_94_doubleclick.java
@@ -1,9 +1,22 @@
 package Selenium_Prastic;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Assignment_94_doubleclick {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ChromeDriver d1 = new ChromeDriver();
+		d1.get("https://www.google.co.in/");
+		d1.manage().window().maximize();
+		
+		WebElement e1=	d1.findElement(By.linkText("Gmail"));
+		Actions a1 = new Actions(d1);
+		//a1.contextClick().perform();
+		a1.doubleClick(e1).perform();
+		
 
 	}
 
